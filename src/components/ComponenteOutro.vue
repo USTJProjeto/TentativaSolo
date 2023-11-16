411a0e4e95d28f39c9976a6ddf8d2258517890f0
André Pereira Russo <andreprusso7@gmail.com>
2023-11-15 22:35:57 -0300

    Dois Componentes conversando

--- src/components/ComponenteOutro.vue
+++ src/components/ComponenteOutro.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h2>Eventos Recebidos:</h2>
-    <div v-for="(evento, index) in eventos" :key="index" :style="{ background: evento.cor }">
-      <strong>Data do Evento:</strong> {{ evento.data }}
-      <br />
-      <strong>Categoria:</strong> {{ evento.categoria }}
-      <hr />
+  <div class="container">
+    <div class="eventos">
+      <h2>Eventos Recebidos:</h2>
+      <div v-for="(evento, index) in eventos" :key="index" class="evento" :style="{ background: evento.cor }">
+        <strong>Data do Evento:</strong> {{ evento.data }}
+        <br />
+        <strong>Categoria:</strong> {{ evento.categoria }}
+        <hr />
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +24,19 @@ export default {
 </script>
 
 <style scoped>
-/* Adicione estilos específicos para este componente se necessário */
+.container {
+  display: flex;
+}
+
+.eventos {
+  flex-grow: 1;
+  padding: 20px;
+}
+
+.evento {
+  margin-bottom: 15px;
+  padding: 10px;
+  border-radius: 4px;
+}
+
 </style>
--- src/components/FormularioEventos.vue
+++ src/components/FormularioEventos.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
-    <h2>Formulário do Evento</h2>
-    <form @submit.prevent="enviarFormulario">
-      <div>
-        <label for="nome">Nome do Evento:</label>
-        <input type="text" id="nome" v-model="evento.nome" required>
-      </div>
-      <div>
-        <label for="data">Data do Evento:</label>
-        <input type="date" id="data" v-model="evento.data" required>
-      </div>
-      <div>
-        <label for="categoria">Categoria:</label>
-        <input type="text" id="categoria" v-model="evento.categoria" required>
-      </div>
-      <div>
-        <label for="cor">Cor:</label>
-        <select id="cor" v-model="evento.cor" required>
-          <option value="vermelho">Vermelho</option>
-          <option value="azul">Azul</option>
-          <option value="verde">Verde</option>
-          <option value="amarelo">Amarelo</option>
-        </select>
-      </div>
-      <button type="submit">Enviar</button>
-    </form>
+  <div class="container">
+    <div class="formulario">
+      <h2>Formulário do Evento</h2>
+      <form @submit.prevent="enviarFormulario">
+        <div class="campo-formulario">
+          <label for="nome">Nome do Evento:</label>
+          <input type="text" id="nome" v-model="evento.nome" required>
+        </div>
+        <div class="campo-formulario">
+          <label for="data">Data do Evento:</label>
+          <input type="date" id="data" v-model="evento.data" required>
+        </div>
+        <div class="campo-formulario">
+          <label for="categoria">Categoria:</label>
+          <input type="text" id="categoria" v-model="evento.categoria" required>
+        </div>
+        <div class="campo-formulario">
+          <label for="cor">Cor:</label>
+          <select id="cor" v-model="evento.cor" required>
+            <option value="vermelho">Vermelho</option>
+            <option value="azul">Azul</option>
+            <option value="verde">Verde</option>
+            <option value="amarelo">Amarelo</option>
+          </select>
+        </div>
+        <button type="submit" class="botao-enviar">Enviar</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -36,31 +38,62 @@ export default {
         nome: '',
         data: '',
         categoria: '',
-        cor: '', 
+        cor: '',
       },
     };
   },
   methods: {
     enviarFormulario() {
-      // Emitir o evento enviarDados com os dados do evento
       this.$emit('enviarDados', this.evento);
-
-      // Restante do código para download do JSON (se necessário)
-      const dadosJSON = JSON.stringify(this.evento, null, 2);
-      const blob = new Blob([dadosJSON], { type: 'application/json' });
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = 'dados_evento.json';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      console.log('Enviando dados:', this.evento);
     },
   },
 };
 </script>
 
 <style scoped>
-/* Adicione estilos específicos para este componente se necessário */
+.container {
+  display: flex;
+}
+
+.formulario {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  background-color: #706c6c;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.campo-formulario {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+  background-color: #007bb5;
+}
+
 </style>
