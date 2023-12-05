dceeea5a78d9334f1b1b90e9cb1ae193eb95a309
Gustavo Duarte <guga9055@gmail.com>
2023-12-05 16:02:11 -0300

    falta microservico composicao

--- src/components/ComponenteOutro.vue
+++ src/components/ComponenteOutro.vue
@@ -2,7 +2,38 @@
   <div class="container">
     <div class="eventos">
       <h2>Tarefas</h2>
-      <button @click="fetchData">Ver Tarefas</button>
+      <div class="btn-ctn">
+        <button @click="fetchData">Ver Tarefas</button>
+        <button id="myBtn" @click="btnClick">Adicionar anotação</button>
+      </div>
+      <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <div class="modal-form-container">
+            <form method="post" :action="formSubmit">
+              <select name="sel-tarefa" v-model="selectedTarefaId" id="tar">
+                <option
+                  v-for="tarefa in tarefaData"
+                  :key="tarefa.id"
+                  :value="tarefa.id"
+                >
+                  {{ tarefa.tarefa.nome }}
+                  {{ tarefa.id }}
+                </option>
+              </select>
+              <label>Anotacão</label>
+              <input
+                type="anotacao"
+                name="anotacao"
+                v-model="anotacao"
+                required
+              />
+              <button type="submit" class="botao-enviar">Criar Anotação</button>
+            </form>
+          </div>
+        </div>
+      </div>
       <div class="container-table">
         <table class="table">
           <thead>
@@ -22,9 +53,10 @@
                   >{{ tarefa.tarefa.categoria }}</span
                 >
               </td>
-              <td>
-                {{ tarefa.tarefa.descricao }}
-              </td>
+              <td>{{ tarefa.id }}</td>
+              <!-- <td v-for="anotacao in anotacaoData"> -->
+              <!--     a -->
+              <!-- </td> -->
             </tr>
           </tbody>
         </table>
@@ -35,16 +67,28 @@
 
 <script>
 import axios from "axios";
-
 export default {
   data() {
     return {
       tarefaData: null,
       anotacaoData: null,
       dataLoaded: false,
+      selectedTarefaId: null,
     };
   },
+  computed: {
+    formSubmit() {
+      console.log(this.selectedTarefaId);
+      return this.selectedTarefaId;
+    },
+  },
   methods: {
+    btnClick() {
+      var modal = document.getElementById("myModal");
+      var btn = document.getElementById("myBtn");
+      var span = document.getElementsByClassName("close")[0];
+      modal.style.display = "block";
+    },
     fetchData() {
       axios
         .get("http://localhost:4000/get/tarefas")
@@ -52,22 +96,19 @@ export default {
           this.tarefaData = response.data;
           this.dataLoaded = false;
 
-          // Use Promise.all to handle multiple asynchronous requests
           const requests = this.tarefaData.map((tarefa) => {
             return axios.get(
-              `http://localhost:5000/get/tarefas/${tarefa.id}/anotacoes`
+              `http://localhost:5000/get/tarefas/{this.selectedTarefaId}/anotacoes`
             );
           });
 
           return Promise.all(requests);
         })
         .then((anotacoesResponses) => {
-          // Handle responses for each anotacoes request
           this.anotacaoData = anotacoesResponses.map(
             (response) => response.data
           );
           this.dataLoaded = true;
-          console.log(this.anotacaoData);
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -78,6 +119,48 @@ export default {
 </script>
 
 <style scoped>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+.btn-ctn {
+  display: flex;
+  justify-content: space-between;
+}
 .badge {
   color: white;
   padding: 4px 8px;
